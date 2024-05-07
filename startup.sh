4933110add4675873f0d9a6db0d8face89fb3acd
Jason Moore <jmoore@austwa.com>
2024-05-07 20:05:36 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -78,10 +78,6 @@ if [ "${CORS_ENABLED}" = "true" ]; then
         <param-name>cors.support.credentials</param-name>\n\
         <param-value>${CORS_SUPPORT_CREDENTIALS}</param-value>\n\
       </init-param>\n\
-      <init-param>\n\
-        <param-name>cors.preflight.maxage</param-name>\n\
-        <param-value>10</param-value>\n\
-      </init-param>\n\
     </filter>\n\
     <filter-mapping>\n\
       <filter-name>DockerGeoServerCorsFilter</filter-name>\n\
